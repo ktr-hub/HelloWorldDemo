dc30018f7b8358055c369189c6236140b858a6ca
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-24 11:12:49 +0530

    [TIrupathi] Refactor. inside Demo

--- ForGit/Program.cs
+++ ForGit/Program.cs
@@ -7,6 +7,7 @@ namespace ForGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Everyonee!");
+            Console.WriteLine("In DemoBranch!");
         }
     }
 }
